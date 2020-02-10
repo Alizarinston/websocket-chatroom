cbbc1d326f8527c6f2ab5116efad89a51c419970
alizarinston <alizarinstone@gmail.com>
2020-02-10 11:39:13 +0200

    timestamp adaptation

--- frontend/src/containers/Chat.js
+++ frontend/src/containers/Chat.js
@@ -40,7 +40,7 @@ class Chat extends React.Component {
         this.setState({
             message: event.target.value
         })
-    }
+    };
 
     sendMessageHandler = (e) => {
         e.preventDefault();
@@ -54,6 +54,29 @@ class Chat extends React.Component {
         });
     };
 
+    renderTimestamp = timestamp => {
+    let prefix = "";
+    const timeDiff = Math.round(
+      (new Date().getTime() - new Date(timestamp).getTime()) / 60000
+    );
+    if (timeDiff < 1) {
+      // less than one minute ago
+      prefix = "just now...";
+    } else if (timeDiff < 60 && timeDiff > 1) {
+      // less than sixty minutes ago
+      prefix = `${timeDiff} minutes ago`;
+    } else if (timeDiff < 24 * 60 && timeDiff > 60) {
+      // less than 24 hours ago
+      prefix = `${Math.round(timeDiff / 60)} hours ago`;
+    } else if (timeDiff < 31 * 24 * 60 && timeDiff > 24 * 60) {
+      // less than 7 days ago
+      prefix = `${Math.round(timeDiff / (60 * 24))} days ago`;
+    } else {
+      prefix = `${new Date(timestamp)}`;
+    }
+    return prefix;
+  };
+
     renderMessages = (messages) => {
         const currentUser = "admin";
         return messages.map((message, i) => (
@@ -63,13 +86,13 @@ class Chat extends React.Component {
                 <img src="http://emilcarlsson.se/assets/mikeross.png" />
                 <p>{message.content}
                     <br />
-                    <small className={message.author === currentUser ? 'sent' : 'replies'}>
-                    {Math.round((new Date().getTime() - new Date(message.timestamp).getTime())/60000)} minutes ago
+                    <small>
+                    {this.renderTimestamp(message.timestamp)}
                     </small>
                 </p>
             </li>
         ));
-    }
+    };
 
     componentDidMount() {
         WebSocketInstance.connect();
